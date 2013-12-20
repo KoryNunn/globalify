8cb02215dd802279ac1e15f33711c943c7ab8429
Kory Nunn <knunn187@gmail.com>
2013-12-20 17:28:48 +1000

    more prodding..

--- globalify.js
+++ globalify.js
@@ -7,45 +7,50 @@ function globalify(){
         resumer = require('resumer'),
         path = require('path'),
         fs = require('fs'),
-        npm = require("npm"),
+        childProcess = require("child_process"),
+        exec = childProcess.exec;
         packageJson = require('./package.json'),
-        moduleName = process.argv[2];
+        moduleName = process.argv[2],
+        version = process.argv[3] || 'x.x.x',
+        outputFileName = moduleName + '-' + version.replace(/\./g,'-') + '.js';
 
     program
       .version(packageJson.version)
       .option('-g, --globalName [globalName]', 'the name of the global to expose')
       .parse(process.argv);
 
-    function globalifyModule(moduleName){
+    function globalifyModule(moduleName, callback){
+
         var stream = resumer().queue('window["' + (program.globalName || moduleName) + '"] = require("' + moduleName + '");').end();
-
-        var b = browserify([stream]);
-        b.bundle().pipe(process.stdout);
+        var b = browserify({
+            entries: [stream],
+            //basedir: packageJson.modulesDirectory
+        });
+        b.bundle(callback).pipe(fs.createWriteStream(outputFileName));
     }
 
-    function instalModule(moduleName, version){
-        npm.load({ prefix: packageJson.modulesDirectory },
-            function (error) {
-                if(!error){
-                    globalifyModule(moduleName);
-                }
-                npm.config.set('dir', packageJson.modulesDirectory);
-
-                npm.commands.install([moduleName], function (error, data) {
-                    if(error){
-                        console.log(error);
-                    }
-
-                    globalifyModule(moduleName);
-                });
-                npm.on("log", function (message) {
-                    console.log(message);
-                });
-
-        })
+    function instalModule(moduleName, version, callback){
+        exec('npm install ' + moduleName + '@"' + version + '"', function(error){
+        //exec('npm --prefix ' + packageJson.modulesDirectory + ' install ' + moduleName + '@"' + version + '"', function(error){
+            callback(error);
+        }).stdout.pipe(process.stdout);
     }
 
-    instalModule(moduleName, 'x.x.x');
+    function globalifyCallback(error, data){
+        console.log(error);
+        if(!error){
+            return;
+        }
+        instalModule(moduleName, version, function(error){
+            if(error){
+                console.log(error);
+                return;
+            }
+            globalifyModule(moduleName, globalifyCallback);
+        });
+    }
+
+    globalifyModule(moduleName, globalifyCallback);
 
 }
 
